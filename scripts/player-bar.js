59ea472f0e90983e7bfd8170f4b6be4055747b97
jewels98 <mrwright0825@gmail.com>
2018-07-13 16:30:19 -0400

    Fixing issues with merge

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
     $('button#play-pause').on('click', function(){
-        player.playPause();
+        helper.playPauseAndUpdate();
         $(this).attr('playState', player.playState);
     });
 
@@ -11,9 +11,21 @@
           const nextSongIndex = currentSongIndex + 1;
           if (nextSongIndex >= album.songs.length) { return; }
           const nextSong = album.songs[nextSongIndex];
-          player.playPause(nextSong);
+          helper.playPauseAndUpdate(nextSong);
+    });
+    
+    $('#time-control input').on('input', function (event) {
+        player.skipTo(event.target.value);
     });
 
+    setInterval(() => {
+    if(player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control.current-time').text(currentTime);
+    $('#time-control input').val(percent);
+}, 1000);
      // Create previous button
     
     $('button#previous').on('click', function () {
@@ -24,19 +36,8 @@
         if (previousSongIndex < 0) { return; }
         
         const previousSong = album.songs[previousSongIndex];
-        player.playPause(previousSong);
+        helper.playPauseAndUpdate(previousSong);
     });
 
-    $('#time-control input').on('input', function (event) {
-        player.skipTo(event.target.value);
-    });
 
-        setInterval(() => {
-        if(player.playState !== 'playing') { return; }
-        const currentTime = player.getTime();
-        const duration = player.getDuration();
-        const percent = (currentTime / duration) * 100;
-        $('#time-control.current-time').text(currentTime);
-        $('#time-control input').val(percent);
-    }, 1000);
 }
